6b1aa836bdd23a4ed7412e38bb6ff623cce7e815
Divyangi <divyangisingh@outlook.com>
2020-05-27 19:12:18 +0530

    Final

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,46 +1,29 @@
 import React,{ useState } from 'react';
-import {Card, CardBody, CardTitle, CardHeader,CardText} from 'reactstrap';
+import {Card, CardBody, CardTitle, CardHeader,CardText, CardFooter} from 'reactstrap';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
 
+const Time  = ({at , startDate}) =>{
 
-const Time  = ({at , startDate}) =>{
-  console.log(startDate)
-    return(<Card style={{padding:"10px"}}>
-    {at.map(attime =>{
-     
-        return(<div className="row">
-        <div className="col-4">
-        <Card style={{width:"100%"}}>
-        <CardText>From : {attime.start_time} </CardText>
-        </Card>
-        </div>
-        <div className="col-4">
-        <Card style={{width:"100%"}}>
-      
-        <CardText>To : {attime.end_time}</CardText>
-        </Card>
-        </div>
-        <div className="col-4" style={{textAlign:"center"}}>
-        <Card>
-        <CardText style={{color:"red"}}>View</CardText>
-        </Card>
-        </div>
-        </div>);
-    
-    })}
-    
-    </Card>)
-  }
-//code  with date filteration
-/*
-const Time  = ({at , startDate}) =>{
-console.log(startDate)
+var selectedDate = Date.parse(startDate);
+console.log(selectedDate);
+
   return(<Card style={{padding:"10px"}}>
   {at.map(attime =>{
-    if(startDate >= (new Date(attime.start_time)) && startDate <= (new Date(attime.end_time))){
-      console.log("startdate:", attime.start_time)
+
+    var startDatee = attime.start_time.split(" ").slice(0,3).join(" ");
+    console.log(startDatee);
+
+    var endDatee = attime.end_time.split(" ").slice(0,3).join(" ");
+
+    console.log(Date.parse(new Date(startDatee)));
+    console.log(Date.parse(new Date(endDatee)));
+
+    var st = Date.parse(new Date(startDatee));
+    var endt = Date.parse(new Date(endDatee));
+
+    if(selectedDate >= st && selectedDate <= endt){
       return(<div className="row">
       <div className="col-4">
       <Card style={{width:"100%"}}>
@@ -68,11 +51,11 @@ console.log(startDate)
   
   </Card>)
 }
-*/
+
 
 const Users = ({ users,loading}) => {
 const [currentUserActivityPeriods , setCurrentUserActivityPeriods] = useState([]);
-const [startDate, setStartDate] = useState(new Date());
+const [startDate, setStartDate] = useState(new Date("Feb 1 2020"));
   console.log(currentUserActivityPeriods)
   if (loading) {
     return <h1>Loading..</h1>;
@@ -92,7 +75,7 @@ const [startDate, setStartDate] = useState(new Date());
       </CardTitle>
       </CardHeader>
       
-      <CardBody>
+      <CardBody style={{borderStyle:"hidden"}}>
       <ul className="list-groupmb-3">
       {users.map(user => (
           <li
@@ -110,50 +93,67 @@ const [startDate, setStartDate] = useState(new Date());
      
       </header>
      
-      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Choose date to View User activity</h5>
-         
-
-            <div class="container">
-    <div class="row">
-        <div class='col'>
-        <DatePicker
-        selected={startDate}
-        onChange={date => setStartDate(date)}
-        showTimeSelect
-        timeFormat="HH:mm"
-        timeIntervals={15}
-        timeCaption="time"
-        dateFormat="MMM d yyyy h:mmaa"
-      />
-        </div>
+        <div className="container" style={{height:"500px", width:"600px",padding:"0",borderRadius:"45px 45px"}}>
+        <Card style={{width:"100%", height:"100%",borderRadius:"35px"}}>
+        <CardHeader>
+        
+        <div class="modal-header" style={{display:"block"}}>
+        
+        <h1 className=" cardheading modal-title" id="exampleModalLabel" style={{textAlign:"center"}}>Users's Activity</h1>
+        <br></br>
+        <div class="container">
+        <h3>Choose  the date </h3>
+<div class="row">
+    <div class='col'>
+    <DatePicker
+    selected={startDate}
+    onChange={date => setStartDate(date)}
+    showTimeSelect
+    timeFormat="HH:mm"
+    timeIntervals={15}
+    timeCaption="time"
+    dateFormat="MMM d yyyy"
+  />
     </div>
 </div>
-<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-<span aria-hidden="true">&times;</span>
-</button>
-          </div>
-          <div class="modal-body">
-          {currentUserActivityPeriods.map(at =>{
-           return <div>
-           <Time at={at} startDate={startDate}></Time>
-           </div>
-           })
-           
-          }
-          </div>
-      
-          </div>
-          <div class="modal-footer">
+</div>
+
+      </div>
+        
+        </CardHeader>
+        <CardBody>
+        <div class="modal-body">
+        {currentUserActivityPeriods.map(at =>{
+         return <div>
+         <Time at={at} startDate={startDate}></Time>
+         </div>
+         })
+         
+        }
+        </div>
+        </CardBody>
+        <CardFooter>
+
+        <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
           </div>
+        </CardFooter>
+        </Card>
+        </div>
+
+
+         
+         
+      
+         
+          
         </div>
       </div>
     </div>
-    
+    </div>
     
     
     
